7cbc34c58a35e4d522e50fd3a9865cde161c2f5e
gangulwar <aarshgangulwar23@gmail.com>
2025-05-13 23:43:59 +0530

    Initial Commit

---
+++ SinceThen/SinceThenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinceThenApp.swift
+//  SinceThen
+//
+//  Created by Gangulwar on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SinceThenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SinceThenTests/SinceThenTests.swift
@@ -0,0 +1,17 @@
+//
+//  SinceThenTests.swift
+//  SinceThenTests
+//
+//  Created by Gangulwar on 5/13/25.
+//
+
+import Testing
+@testable import SinceThen
+
+struct SinceThenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
